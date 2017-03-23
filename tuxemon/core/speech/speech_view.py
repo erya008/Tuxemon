e7323014212f2763aa439568c96561d3e6965873
reedavers <reedavers@yahoo.com>
2017-03-23 17:49:59 -0400

    text positioning

--- tuxemon/core/speech/speech_view.py
+++ tuxemon/core/speech/speech_view.py
@@ -3,13 +3,14 @@ import pygame
 class SpeechView:
     screen_text = "no speech text here"
 
+
     def __init__(self):
         self.voice_text = "no previous voice text"
 
     def draw(self, screen):
-        self.font = pygame.font.SysFont('Arial', 36)
-
-        screen.blit(self.font.render(self.voice_text, True, (0,0,0)), (200, 100))
+        font_color = (255,140,0)
+        self.font = pygame.font.SysFont('Arial', 36, bold=True)
+        screen.blit(self.font.render(self.voice_text, True, font_color), (100, 600))
         #print("Drawing inside speech view")
 
     def notify(self, other):
